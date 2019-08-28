7527ea4ec6d97cea8ca6ec153b0fc840789d920f
prasad <prasad.k@krds.fr>
2019-08-28 12:55:52 +0530

    asdsad

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 mysql -u root -h localhost  -P 3306 --password=""  -e "CREATE USER 'jobseek'@'localhost' IDENTIFIED BY 'jobseek'; "
 mysql -u root -h localhost  -P 3306 --password=""  -e "CREATE DATABASE jobseek; GRANT ALL PRIVILEGES ON *.* TO 'jobseek'@'localhost' IDENTIFIED BY 'jobseek'"
 mysql -u jobseek -h localhost -P 3306 --password="jobseek" -e "USE jobseek;"
-mysql -u jobseek -h localhost -P 3306 --password="jobseek" -D lba < /var/www/html/dump.sql
+mysql -u jobseek -h localhost -P 3306 --password="jobseek" -D jobseek < /var/www/html/dump.sql
 exec "$@"
